30a17d44aff8f0565360b18c0b7c8c19cec42e05
JValentino <valentino13020@gmail.com>
2025-11-02 20:27:56 +0700

    02-11-2025

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Filamet</title>
+</head>
+<body>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <h1 class="text-4xl font-bold mb-4">Welcome to Filamet</h1>
+        <p class="text-lg">Your go-to platform for managing and sharing your 3D printing files.</p>
+    </div>
+</body>
+</html>
